a17687b6ac3a53921f7a26a12bdb6eed577f69a4
rachelruff <rachelruff@gmail.com>
2018-11-12 12:55:30 -0700

    connected to massive db

--- functions/index.js
+++ functions/index.js
@@ -1,61 +1,34 @@
-const functions = require('firebase-functions');
-const cors = require('cors');
-const { json } = require('body-parser');
-const express = require('express');
+const functions = require("firebase-functions");
+const cors = require("cors");
+const { json } = require("body-parser");
+const express = require("express");
+const massive = require("massive");
 
+const mainCtrl = require("./controllers/mainCtrl");
+
+const port = 3001;
 
 const app = express();
 
 app.use(cors({ origin: true }));
 app.use(json());
 
+const secrets = require("./secrets.js");
 
-app.get('/api/tester', (req, res) => {res.send('You hit my endpoint!')})
+massive(secrets.connectionString)
+  .then(db => {
+    app.set("db", db);
+  })
+  .catch(console.log, '*******');
 
+app.get("/api/tester", (req, res) => {
+  res.send("You hit my endpoint!");
+});
 
- 
-const PORT = 3001;
+app.get("/api/getAllEmployees", mainCtrl.getAllEmployees);
 
-app.listen(PORT || 3001, () => {
-  console.log(`App listening on port ${PORT || 3001}!`);
+app.listen(port || 3001, () => {
+  console.log(`App listening on port ${port || 3001}!`);
 });
 
 exports.app = functions.https.onRequest(app);
-
-
-
-
-// const functions = require("firebase-functions");
-// const cors = require("cors");
-// const { json } = require("body-parser");
-// const express = require("express");
-// const massive = require("massive");
-
-// const mainCtrl = require("./controllers/mainCtrl");
-
-// const port = 3001;
-
-// const app = express();
-
-// app.use(cors({ origin: true }));
-// app.use(json());
-
-// const secrets = require("./secrets.js");
-
-// massive(secrets.connectionString)
-//   .then(db => {
-//     app.set("db", db);
-//   })
-//   .catch(console.log, '*******');
-
-// app.get("/api/tester", (req, res) => {
-//   res.send("You hit my endpoint!");
-// });
-
-// app.get("/api/getAllEmployees", mainCtrl.getAllEmployees);
-
-// app.listen(port || 3001, () => {
-//   console.log(`App listening on port ${port || 3001}!`);
-// });
-
-// exports.app = functions.https.onRequest(app);
